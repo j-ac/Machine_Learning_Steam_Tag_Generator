98cbafecc2ea372802366bc4f13e2b5ae17a688a
ABDO-A <78626612+ABDO-A@users.noreply.github.com>
2023-03-23 17:38:57 -0400

    Documentation added to code along with some clean up

--- functs.py
+++ functs.py
@@ -1,7 +1,9 @@
+# Helper functions for data processing
 import numpy as np
 import random
 import pandas as pb
 
+# Conversion of training/testing set into numpy ndarray
 def npFloatArray(trainSet):
     train_x_list = np.zeros(trainSet.shape[0])
     for i in trainSet:
